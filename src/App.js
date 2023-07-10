4d00266792bb37f3d4831fbdace393244edd7193
Vaibhav Matere <vpm00718897@gmail.com>
2023-07-10 16:49:33 +0530

    basic refactor

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import "./App.css"
 import { Application } from './application';
+import { Skills } from "./components/Skills";
 
 function App() {
+   const skills = ["html", "css", "js", "react"];
   return (<div className="App">
     <Application />
+    <Skills skills={skills} />
     </div>
   );
 }
